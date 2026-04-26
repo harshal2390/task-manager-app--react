704cb061f301d63c81c9d67ecd838bdbf37dc5e7
harshal2390 <harshalchaudhari534@gmail.com>
2026-04-26 18:11:26 +0530

    remove unessary comments

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -8,7 +8,6 @@ function Filters({
 }) {
   return (
     <div className="max-w-2xl mx-auto mt-6 bg-white shadow-lg rounded-xl p-4 flex flex-col md:flex-row gap-3 items-center">
-      {/* Search */}
       <input
         type="text"
         placeholder="Search tasks..."
@@ -17,7 +16,6 @@ function Filters({
         className="flex-1 w-full border border-gray-300 rounded-lg px-4 py-2.5 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
 
-      {/* Priority */}
       <select
         value={filterPriority}
         onChange={(e) => setFilterPriority(e.target.value)}
@@ -29,7 +27,6 @@ function Filters({
         <option value="Low">Low</option>
       </select>
 
-      {/* Category */}
       <select
         value={filterCategory}
         onChange={(e) => setFilterCategory(e.target.value)}
@@ -41,7 +38,6 @@ function Filters({
         <option value="Personal">Personal</option>
       </select>
 
-      {/* Reset */}
       <button
         onClick={() => {
           setSearch("");
